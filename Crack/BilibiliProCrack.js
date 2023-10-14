69596c26d37f499194504d147f2256740c592a84
Sliverkiss <1393579810@qq.com>
2023-10-14 12:26:49 +0800

    Create BilibiliProCrack.js

---
+++ Crack/BilibiliProCrack.js
@@ -0,0 +1,13 @@
+var modifiedHeaders = $request['headers'];
+modifiedHeaders['Cookie'] = '';
+modifiedHeaders['x-bili-device-bin'] = '';
+modifiedHeaders['Authorization'] = '';
+modifiedHeaders['User-Agent'] = '';
+modifiedHeaders['buvid'] = '';
+modifiedHeaders['x-bili-metadata-bin'] = '';
+modifiedHeaders['x-bili-locale-bin'] = '';
+modifiedHeaders['x-bili-network-bin'] = '';
+modifiedHeaders['x-bili-fawkes-req-bin'] = '';
+modifiedHeaders['x-bili-trace-id'] = '';
+modifiedHeaders['x-bili-exps-bin'] = '';
+$done({'headers': modifiedHeaders});
