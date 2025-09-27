9acb9ebeb1075bd758721c69a534c2c5b26f40ec
Sliverkiss <1393579810@qq.com>
2025-09-27 20:54:15 +0800

    Update tuhu.js

--- Script/tuhu.js
+++ Script/tuhu.js
@@ -203,7 +203,7 @@ async function checkin(type, name) {
 
     var result = await Request(opt);
     if (result?.Code == 1) {
-        msg += `${name}任务: 签到成功, 积分 +${result?.data?.rewardIntegral}, 连续签到: ${result?.data?.rewardIntegral}/7天 ✅`;
+        msg += `${name}任务: 签到成功, 积分 +${result?.data?.rewardIntegral}, 连续签到: ${result?.data?.continuousDays}/7天 ✅`;
     } else {
         msg += `${name}任务: 签到失败, ${result?.message || $.toStr(result)}`;
     }
